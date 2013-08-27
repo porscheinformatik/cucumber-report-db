c77940ccc5aa7ba4b81c855104e0a8f7127dc8ab
Peyman Aparviz <Pymnpe@gmail.com>
2013-08-27 17:29:20 +0200

    configured relative path for server url

--- cucumber-report-web/src/main/webapp/js/config.js
+++ cucumber-report-web/src/main/webapp/js/config.js
@@ -1,4 +1,4 @@
-var serverUrl = 'http://atbghx0017:8081/rest/';
+var serverUrl = '/rest/';
 var queryBaseUrl = serverUrl + 'query/bddReports/';
 var collectionBaseUrl = serverUrl + 'collection/bddReports/';
 var fileBaseUrl = serverUrl + 'file/bddReports/';
