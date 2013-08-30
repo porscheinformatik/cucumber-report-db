3aee3dfc313720cd530c5b0a00cafb014a85fc9f
stefanmayer <s.mayer@porscheinformatik.at>
2013-08-30 10:36:48 +0200

    added silk-bdd-result-plugin module (for BDD result integration in Silk Central)

---
+++ silk-bdd-result-plugin/src/main/java/at/porscheinformatik/common/utils/silkplugin/dto/DateDTO.java
@@ -0,0 +1,17 @@
+package at.porscheinformatik.common.utils.silkplugin.dto;
+
+public class DateDTO
+{
+    private String $date;
+
+    public String get$date()
+    {
+        return $date;
+    }
+
+    public void set$date(String $date)
+    {
+        this.$date = $date;
+    }
+
+}
