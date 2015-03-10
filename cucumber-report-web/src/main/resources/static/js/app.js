57d8fbfe6af7e79a899d1781750e0b3f1420d4e4
Stefan Huber <extern.stefan.huber@porscheinformatik.at>
2015-03-10 11:31:43 +0100

    Adding Support for MimeType "application/zip"

--- cucumber-report-web/src/main/resources/static/js/app.js
+++ cucumber-report-web/src/main/resources/static/js/app.js
@@ -593,6 +593,25 @@
         $scope.isEmbeddedVideo = function (mimeType) {
             return mimeType === 'video/mp4';
         };
+		$scope.isEmbeddedZIP = function (mimeType) {
+			return mimeType === 'application/zip';
+		};
+
+		$scope.downloadZipFile = function(featurename, scenarioname, embedded) {
+			var scope = $rootScope.$new(true);
+			scope.getEmbedding = $scope.getEmbedding;
+			scope.embedded = embedded;
+			var url = fileBaseUrl + $routeParams.colName + '/' + embedded.url + '/';
+
+			var downloadLink = document.createElement("a");
+			downloadLink.href = url;
+			downloadLink.download = featurename+'-'+scenarioname+'.zip';
+
+			document.body.appendChild(downloadLink);
+			downloadLink.click();
+			document.body.removeChild(downloadLink);
+
+		};
 
         $scope.embeddingLightbox = function(embedded) {
           var scope = $rootScope.$new(true);
