b08033fe280253bb60eccfb692c4f8224c17b9a2
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2015-03-17 22:53:31 +0100

    Error getting ranking and charts if collection has empty items #27

--- cucumber-mongodb-rest/src/main/resources/mapMostExecutedSteps.js
+++ cucumber-mongodb-rest/src/main/resources/mapMostExecutedSteps.js
@@ -1,9 +1,11 @@
-function() {this.features.forEach(function(feature) { 
-	feature.scenarios.forEach(function(scenario) {
-		scenario.steps.forEach(function(step) {
-			if (step.result.status==="failed" || step.result.status==="passed" ){
-				emit(step.name, 1);
-			}
-		})
-	})
+function() {this.features.forEach(function(feature) {
+    if(feature.scenarios !== undefined) {
+        feature.scenarios.forEach(function(scenario) {
+            scenario.steps.forEach(function(step) {
+                if (step.result.status==="failed" || step.result.status==="passed" ){
+                    emit(step.name, 1);
+                }
+            })
+        })
+    }
 })}
--- cucumber-mongodb-rest/src/main/resources/mapStepsDurations.js
+++ cucumber-mongodb-rest/src/main/resources/mapStepsDurations.js
@@ -1,9 +1,11 @@
-function() {this.features.forEach(function(feature) { 
-	feature.scenarios.forEach(function(scenario) {
-		scenario.steps.forEach(function(step) {
-			if (step.result.status==="failed" || step.result.status==="passed" ){
-				emit(step.name, step.result.duration);
-			}
-		})
-	})
+function() {this.features.forEach(function(feature) {
+    if(feature.scenarios !== undefined) {
+        feature.scenarios.forEach(function (scenario) {
+            scenario.steps.forEach(function (step) {
+                if (step.result.status === "failed" || step.result.status === "passed") {
+                    emit(step.name, step.result.duration);
+                }
+            })
+        })
+    }
 })}
